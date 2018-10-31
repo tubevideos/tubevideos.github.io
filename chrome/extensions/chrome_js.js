5dd889b3f89edd3739c66839fa6176155a833151
tubevideos <44216889+tubevideos@users.noreply.github.com>
2018-10-31 09:16:17 +0800

    Update chrome/extensions/chrome_js.js

--- chrome/extensions/chrome_js.js
+++ chrome/extensions/chrome_js.js
@@ -20,62 +20,65 @@ $(window).ready(function(){
  *  修改view
  */
 function modifyView(str, time){
-    header(time);
+    addHeader(time);
     var arr = str.split("/");
     if(arr[4]==='detail'){// 详情页
-		button(time);//加button按钮
-        detailFooter(time);//尾部添加
+        addButton(time);//加button按钮
+        addDetailFooter(time);//尾部添加
     } else {
-        sidebar(time);//去除dom
-        footer(time);//尾部添加
+        modifySidebar(time);//去除dom
+        addFooter(time);//尾部添加
     }
 }
 
 /**
  * 修改导航栏
  */
-function header(time) {
-	//判断导航是否已经加载出来
+function addHeader(time) {
+    //判断导航是否已经加载出来
     var currentTime = new Date().getTime();
     if($('#my_navigate').length ===0 && currentTime <= time + validityPeriod){ // 未添加导航,并且在有效期内
         var title = $('.O-j-k>.O-j');
         if(title.length === 0){
-            setTimeout(function (){header(time)}, 1000);
+            setTimeout(function (){addHeader(time)}, 1000);
         }else{
             $('.O-j>.O-j-p-J').remove();
             $('.O-j-gb>.A8AIPb>.PNF6le').html('Chrome插件网');
             $('.O-j-k>.O-j').append('<div id="my_navigate" style="float:right; width:638px;height:100%;margin-right:80px; flex-direction:row; display:inline-flex; justify-content:space-between; align-items:center;"><div class="O-j-gb"><a class="A8AIPb" href="#"><span class="PNF6lea" style="font-size:16px; color: #5f6368;">首页</span></a></div><div class="O-j-gb"><a class="A8AIPb" href="#"><span class="PNF6lea" style="font-size:16px; color:#5f6368;">分类</span></a></div><div class="O-j-gb"><a class="A8AIPb" href="#"><span class="PNF6lea" style="font-size:16px; color:#5f6368;">推荐</span></a></div><div class="O-j-gb"><a class="A8AIPb" href="#"><span class="PNF6lea" style="font-size:16px; color:#5f6368;">百科</span></a></div><div class="O-j-gb"><a class="A8AIPb" href="#" ><span class="PNF6lea" style="font-size:16px; color:#5f6368;">排行</span></a></div><div class="O-j-gb"><a class="A8AIPb" href="#" ><span class="PNF6lea" style="font-size:16px; color:#5f6368;">新闻中心</span></a></div><div class="O-j-gb"><a class="A8AIPb" href="#" ><span class="PNF6lea" style="font-size:16px; color:#5f6368;">关于我们</span></a></div></div>');
-			headerHover();
-		}
+            headerHover();
+        }
     }
 }
 
-function headerHover(){//为title导航添加鼠标悬乎效果
-	$(".O-j-gb>.A8AIPb>.PNF6lea").hover(
-		function () {
-			$(this).css("color","#1a73e8");
-		},
-		function () {
-			$(this).css("color","#5f6368");
-		}
-	);
+/**
+ * 为title导航添加鼠标悬乎效果
+ */
+function headerHover(){
+    $(".O-j-gb>.A8AIPb>.PNF6lea").hover(
+        function () {
+            $(this).css("color","#1a73e8");
+        },
+        function () {
+            $(this).css("color","#5f6368");
+        }
+    );
 }
 
 /**
  * 修改侧边菜单
  */
-function sidebar(time){
+function modifySidebar(time){
     // 判断sidebar是否已经修改
     var currentTime = new Date().getTime();
     if($('#my_sidebar').length === 0 && currentTime <= time + validityPeriod){//未修改,并且在有效期内
         var sidebar = $('.F-n-J>.HWJfBb');
         if(sidebar.length === 0){
-            setTimeout(function(){sidebar(time)} , 1000)
+            setTimeout(function(){modifySidebar(time)} , 1000)
         }else{
             $('.HWJfBb>.gpx3nd').remove();
             $('.HWJfBb>.si3MDf').remove();//下条款
             $('.WrIA6d').next().remove();//下边线
-			sidebar.append('<div id = "my_sidebar"></div>')
+            sidebar.append('<div id = "my_sidebar"></div>')
         }
     }
 
@@ -84,13 +87,13 @@ function sidebar(time){
 /**
  * 尾部添加
  */
-function footer(time){
+function addFooter(time){
     // 判断尾部是否已经添加
     var currentTime = new Date().getTime();
     if($('#my_footer').length ===0 && currentTime <= time + validityPeriod){// 未添加
         var footer = $('.F-ia-k');
         if(footer.length === 0){
-            setTimeout(function (){footer(time)},1000);
+            setTimeout(function (){addFooter(time)},1000);
         }else{
             $('.F-ia-k').append('<div id="my_footer" class="fusion-footer"><footer id="footer" class="fusion-footer-copyright-area" style="padding: 18px 10px 12px;border-top: 1px solid #4b4c4d;background: #282a2b;border-top: 1px solid #4b4c4d;"><div class="fusion-row" style="max-width: 1200px;padding-left: 0px;padding-right: 0px; margin: 0 auto;zoom: 1;"><div class="fusion-copyright-content"><div class="fusion-copyright-notice" style="padding-bottom: 0px;"><div style="color: #ffffff; font: 13px/20px PTSansRegular,Arial,Helvetica,sans-serif;font-weight: 400;">Copyright 2012 - 2018 Avada | All Rights Reserved | Powered by<a href="http://wordpress.org" style="color: #ffffff;text-decoration:none;">WordPress</a> |<a href="http://theme-fusion.com" style="color: #ffffff;text-decoration:none;">Theme Fusion</a></div></div></div></div></footer></div>');
         }
@@ -101,13 +104,13 @@ function footer(time){
 /**
  * 详情页尾部添加
  */
-function detailFooter(time){
+function addDetailFooter(time){
     // 判断尾部是否已经添加
     var currentTime = new Date().getTime();
     if($('#my_detail_footer').length === 0 && currentTime <= time + validityPeriod){// 未添加
         var footer = $('.e-f-ra-gj');
         if(footer.length === 0){
-            setTimeout(function (){detailFooter(time)}, 3000);
+            setTimeout(function (){addDetailFooter(time)}, 3000);
         }else{
             $('.e-f-ra-gj').after('<div id="my_detail_footer" class="fusion-footer"><footer id="footer" class="fusion-footer-copyright-area" style="padding: 18px 10px 12px;border-top: 1px solid #4b4c4d;background: #282a2b;border-top: 1px solid #4b4c4d;"><div class="fusion-row" style="max-width: 1200px;padding-left: 0px;padding-right: 0px; margin: 0 auto;zoom: 1;"><div class="fusion-copyright-content"><div class="fusion-copyright-notice" style="padding-bottom: 0px;"><div style="color: #ffffff; font: 13px/20px PTSansRegular,Arial,Helvetica,sans-serif;font-weight: 400;">Copyright 2012 - 2018 Avada | All Rights Reserved | Powered by<a href="http://wordpress.org" style="color: #ffffff;text-decoration:none;">WordPress</a> |<a href="http://theme-fusion.com" style="color: #ffffff;text-decoration:none;">Theme Fusion</a></div></div></div></div></footer></div>');
         }
@@ -117,13 +120,13 @@ function detailFooter(time){
 /**
  * 安装按钮
  */
-function button(time){
+function addButton(time){
     //判断button是否添加
     var currentTime = new Date().getTime();
     if($('#my_button').length === 0 && currentTime <= time + validityPeriod) {
         var button = $('.h-e-f-Ra-c');
         if(button.length === 0){
-            setTimeout(function (){button(time)}, 1000);
+            setTimeout(function (){addButton(time)}, 1000);
         }else{
             $('.h-e-f-Ra-c').html('<div id="my_button" role="button" class="dd-Va g-c-wb g-eg-ua-Uc-c-za g-c g-c-Sc-ci" aria-label="Remove from Chrome" tabindex="0" style="user-select: none;"><div class="g-c-Hf"><div class="g-c-x"><div class="g-c-R  webstore-test-button-label">添加至 Chrome</div></div></div></div>');
             downloadAlert();
